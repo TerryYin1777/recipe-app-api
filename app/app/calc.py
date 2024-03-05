6bcc8df02a6c6d3cb8b274427dc460ec25fd917a
terryyin1777@gmail.com <terryyin1777@gmail.com>
2024-03-05 10:31:48 -0500

    install psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Simple Calc function
+"""
+
+def add(x, y):
+    """
+    returns addition of x and y
+    """
+    return x + y
+
+def minus(x, y):
+    """
+    minus y from x
+    """
+    return x - y
