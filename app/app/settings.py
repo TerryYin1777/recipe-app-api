1416cb979107223a10eafd7bacb9f901597a83a8
terryyin1777@gmail.com <terryyin1777@gmail.com>
2024-03-18 14:54:08 -0400

    checkpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'recipe',
+    'user',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
 ]
 
